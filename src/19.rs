a39be22ce02d1713c4318102d57dd134bb8d2075
worcestermargreiter621 <worcestermargreiter621@gmail.com>
2025-03-24 13:02:33 +0000

    Add 19.rs

---
+++ src/19.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Example Rust program to add two numbers.
+    let num1 = 5;
+    let num2 = 3;
+    let sum = num1 + num2;
+    println!("The sum of {} and {} is {}", num1, num2, sum);
+}
